8284189116b2e21dd0100e53b9356171237de84c
Florin Mirica <miricaflorin@hotmail.com>
2019-02-24 13:45:50 -0800

    add test for httpget methods

---
+++ WordCounter.Tests/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WordCounter.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WordCounter.Tests.ControllerTests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index_ReturnsCorrectView_True()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ActionResult indexView = controller.Index();
+
+            //Assert
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
+    }
+}
---
+++ WordCounter.Tests/ControllerTests/WordCounterControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WordCounter.Controllers;
+
+namespace WordCounter.Tests.ControllerTests
+{
+    [TestClass]
+    public class WordCounterControllerTest
+    {
+        [TestMethod]
+        public void Index_ReturnsCorrectView_True()
+        {
+            //Arrange
+            WordCounterController controller = new WordCounterController();
+
+            //Act
+            ActionResult indexView = controller.Index();
+
+            //Assert
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
+    }
+}
